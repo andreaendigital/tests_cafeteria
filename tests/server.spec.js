1a310855eaa15ea06418e922c001d7e883465afa
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-10-27 20:31:42 -0300

    primer test validado

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,13 @@
 const request = require("supertest");
 const server = require("../index");
+const app = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
+
+    it("Devuelve estado 200", async () => {
+        const { statusCode } = await request(app).get("/cafes").send();
+        expect(statusCode).toBe(200);
+      });
+    
 
 });
