757ad1d2078fcd53ae157a8d28e51c27f21778ea
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-10-27 21:01:21 -0300

    requerimientos del desafio revisados

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -19,4 +19,34 @@ describe("Operaciones CRUD de cafes", () => {
         const response = await request(app).get("/cafes");
         expect(response.body.length).toBeGreaterThan(0);
       });
+
+      it("Devuelve 404 al intentar eliminar un café con un id que no existe.", async () => {
+        const response = await request(app)
+          .delete(`/cafes/60`)
+          .set("Authorization", `Bearer`);
+        expect(response.status).toBe(404);
+      });
+    
+      it("POST/cafes Agregar un nuevo café y devolver un código 201", async () => {
+        const nuevoCafe = {
+          nombre: "Café Colombiano",
+        };
+        const response = await request(app).post("/cafes").send(nuevoCafe);
+        expect(response.status).toBe(201);
+        expect(response.body).toContainEqual(nuevoCafe);
+      });
+
+     
+        it("Devolver un code 400 en ruta PUT /cafes/:id si los parámetros no coinciden", async () => {
+          const cafeActualizado = {
+            id: 1,
+            nombre: "Café Irish",
+            descripcion:
+              "Mezcla de whisky irlandés, una cucharada de azúcar y café, pero finalmente va cubierto por dos centímetros de crema de leche",
+          };
+          const response = await request(app).put("/cafes/2").send(cafeActualizado);
+          expect(response.status).toBe(400);
+        });
+   
+
 });
